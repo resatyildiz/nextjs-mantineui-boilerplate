ba27bf0757f15451a09a1efe5a9b08a354d98868
Reşat YILDIZ <resat328@gmail.com>
2022-10-16 21:55:39 +0300

    [T-03] Create HeroBullets and import herovocer svg

---
+++ components/HeroBullets/HeroBullets.tsx
@@ -0,0 +1,70 @@
+import {
+  createStyles,
+  Image,
+  Container,
+  Title,
+  Button,
+  Group,
+  Text,
+  List,
+  ThemeIcon,
+} from '@mantine/core';
+import { IconCheck } from '@tabler/icons';
+import { useHeroBulletStyle } from './HeroBullets.style';
+
+
+
+export function HeroBullets() {
+  const { classes } = useHeroBulletStyle();
+  return (
+    <div>
+      <Container>
+        <div className={classes.inner}>
+          <div className={classes.content}>
+            <Title className={classes.title}>
+              A <span className={classes.highlight}>modern</span> React <br /> website boilerplate
+            </Title>
+            <Text color="dimmed" mt="md">
+              Build fully functional accessible web applications faster than ever – Mantine includes
+              more than 120 customizable components and hooks to cover you in any situation
+            </Text>
+
+            <List
+              mt={30}
+              spacing="sm"
+              size="sm"
+              icon={
+                <ThemeIcon size={20} radius="xl">
+                  <IconCheck size={12} stroke={1.5} />
+                </ThemeIcon>
+              }
+            >
+              <List.Item>
+                <b>TypeScript based</b> – build type safe applications, all components and hooks
+                export types
+              </List.Item>
+              <List.Item>
+                <b>Free and open source</b> – all packages have MIT license, you can use Mantine in
+                any project
+              </List.Item>
+              <List.Item>
+                <b>No annoying focus ring</b> – focus ring will appear only when user navigates with
+                keyboard
+              </List.Item>
+            </List>
+
+            <Group mt={30}>
+              <Button radius="xl" size="md" className={classes.control}>
+                Get started
+              </Button>
+              <Button variant="default" radius="xl" size="md" className={classes.control}>
+                Source code
+              </Button>
+            </Group>
+          </div>
+          <Image src={"/images/heroCover.svg"} className={classes.image} alt="Hero Bullets Image" />
+        </div>
+      </Container>
+    </div>
+  );
+}
