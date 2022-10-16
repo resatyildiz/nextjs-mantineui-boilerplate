12407de41754e16adc18660b6774a859161e180d
Reşat YILDIZ <resat328@gmail.com>
2022-10-16 18:58:50 +0300

    [T-02] edit carousel image position and it's data file

---
+++ constants/carouselData.tsx
@@ -0,0 +1,37 @@
+interface CarouselData {
+    image: string;
+    title: string;
+    category: string;
+};
+export const carouselData: Array<CarouselData> = [
+    {
+        image: '/images/slide1.jpg',
+        title: 'Best forests to visit in North America',
+        category: 'nature',
+    },
+    {
+        image: '/images/slide1.jpg',
+        title: 'Hawaii beaches review: better than you think',
+        category: 'beach',
+    },
+    {
+        image: '/images/slide1.jpg',
+        title: 'Mountains at night: 12 best locations to enjoy the view',
+        category: 'nature',
+    },
+    {
+        image: '/images/slide1.jpg',
+        title: 'Aurora in Norway: when to visit for best experience',
+        category: 'nature',
+    },
+    {
+        image: '/images/slide1.jpg',
+        title: 'Best places to visit this winter',
+        category: 'tourism',
+    },
+    {
+        image: '/images/slide1.jpg',
+        title: 'Active volcanos reviews: travel at your own risk',
+        category: 'nature',
+    },
+];
